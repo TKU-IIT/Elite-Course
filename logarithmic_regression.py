8d128126a4206815cffbbb2fde89196cb97fc7e8
Cangyu Lin <jefflin.je598@gmail.com>
2020-04-22 11:13:18 +0800

    add complete informations

--- logarithmic_regression.py
+++ logarithmic_regression.py
@@ -1,5 +1,57 @@
 import csv
-with open('graduate_admissions.csv') as file:
-    csv_reader = csv.reader(file, delimiter=',')
-    for row in csv_reader:
-        print(row)
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+
+raw_data = []
+X = []
+Y = []
+
+with open('graduate-admissions/graduate_admissions.csv', mode='r') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	for row in csv_reader:
+		# print(row)
+		raw_data.append(row)
+
+raw_data = raw_data[1:]
+# print(raw_data)
+
+for row in raw_data:
+	X.append([int(row[1]), int(row[2])])
+	Y.append(int(row[-1]))
+# print(X)
+# print(Y)
+
+admit_gre = []
+admit_toefl = []
+reject_gre = []
+reject_toefl = []
+
+for (gre, toefl), admit in zip(X,Y):
+	if admit == 1:
+		admit_gre.append([gre])
+		admit_toefl.append([toefl])
+	else:
+		reject_gre.append([gre])
+		reject_toefl.append([toefl])
+
+plt.plot(admit_gre, admit_toefl, 'go', label='Admit', alpha=0.8)
+plt.plot(reject_gre, reject_toefl, 'ro', label='Reject', alpha=0.8)
+
+clf = LogisticRegression().fit(X, Y)
+print('Coefficients: ', clf.coef_)
+print('Intercept: ', clf.intercept_)
+
+#Predict the probability of gre: 320, toefl 100 for admission
+print(clf.predict([[320, 100]]))
+print(clf.predict_proba([[320, 100]]))
+
+def f(x):
+	return -(clf.intercept_/clf.coef_[0][1]) - (clf.coef_[0][0]/clf.coef_[0][1])*x
+
+#Generating the graph
+plt.plot([340, 285], [f(340), f(285)])
+
+plt.xlabel('GRE')
+plt.ylabel('TOEFL')
+plt.legend()
+plt.show()
